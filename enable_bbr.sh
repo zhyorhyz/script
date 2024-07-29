a505a0960196558addf00562583960abed7a2250
zhyyy <115544695+zhyorhyz@users.noreply.github.com>
2024-07-29 10:21:41 +0800

    Update enable_bbr.sh

--- enable_bbr.sh
+++ enable_bbr.sh
@@ -3,21 +3,19 @@
 # 设定最低内核版本
 REQUIRED_KERNEL_VERSION="4.9.0"
 
-# 检查并安装必要工具
-if ! command -v dpkg &> /dev/null; then
-    echo "dpkg 工具未安装，正在安装..."
-    sudo apt-get update
-    sudo apt-get install -y dpkg
-fi
-
-if ! command -v grep &> /dev/null; then
-    echo "grep 工具未安装，正在安装..."
-    sudo apt-get update
-    sudo apt-get install -y grep
-fi
+# 更新系统并安装必要工具
+echo "更新系统和安装必要工具..."
+sudo apt-get update
+for tool in dpkg grep; do
+    if ! command -v $tool &> /dev/null; then
+        echo "$tool 工具未安装，正在安装..."
+        sudo apt-get install -y $tool
+    fi
+done
 
 # 检查内核版本
 current_kernel_version=$(uname -r | cut -d "-" -f 1)
+echo "当前内核版本: $current_kernel_version"
 if ! dpkg --compare-versions "$current_kernel_version" ge "$REQUIRED_KERNEL_VERSION"; then
     echo "当前内核版本 $current_kernel_version 不满足要求。请升级到至少 $REQUIRED_KERNEL_VERSION 版本。"
     exit 1
@@ -25,15 +23,17 @@ fi
 
 # 检查是否已启用 BBR
 bbr_enabled=$(sysctl net.ipv4.tcp_congestion_control | grep bbr)
-
 if [[ -z "$bbr_enabled" ]]; then
     echo "BBR 模块未启用，正在启用..."
     
     # 加载 BBR 模块
-    sudo modprobe tcp_bbr
+    if ! sudo modprobe tcp_bbr; then
+        echo "加载 BBR 模块失败。"
+        exit 1
+    fi
     
     # 设置 BBR 为默认的拥塞控制算法
-    echo "net.ipv4.tcp_congestion_control=bbr" | sudo tee -a /etc/sysctl.conf
+    echo "net.ipv4.tcp_congestion_control=bbr" | sudo tee -a /etc/sysctl.conf > /dev/null
     sudo sysctl -p
 
     # 检查 BBR 是否已成功加载
